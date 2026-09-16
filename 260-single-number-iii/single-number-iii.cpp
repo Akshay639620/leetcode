059e71f18b9c98ed129a38ee5d2da24aac903319
Akshay <akshaysrivastav19@gmail.com>
2026-09-17 00:28:54 +0530

    Time: 0 ms (100.00%) | Memory: 14 MB (56.79%) - LeetSync

--- 260-single-number-iii/single-number-iii.cpp
+++ 260-single-number-iii/single-number-iii.cpp
@@ -5,7 +5,7 @@ public:
         for (int i = 0; i < nums.size(); i++) {
             XOR = XOR^ nums[i];
         }
-        int rightShift = XOR & (-XOR);
+        int rightShift = (XOR & (XOR-1)) ^ XOR;
         int b1 = 0,b2 = 0;
         for(int i = 0 ;i<nums.size();i++){
             if(nums[i] & rightShift) b1 =b1 ^ nums[i];
