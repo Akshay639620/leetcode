303e2c5fbcf9b4abb10092131ec2053aa69ca605
Akshay <akshaysrivastav19@gmail.com>
2026-09-19 10:16:10 +0530

    Time: 0 ms (100.00%) | Memory: 7.8 MB (54.52%) - LeetSync

--- 397-integer-replacement/integer-replacement.cpp
+++ 397-integer-replacement/integer-replacement.cpp
@@ -2,15 +2,17 @@ class Solution {
 public:
     int integerReplacement(int n) {
         long long x = n;
-        long long cnt = 0;
+        int cnt = 0;
         while (x > 1) {
-            if (x % 2 == 0) {
-                x = x / 2;
+            if ((x & 1) == 0) {
+                x = x >> 1;
             } else {
-                if (x == 3 || (x + 1) % 4 != 0)
-                    x = x - 1;
+                if (x == 3)
+                    x--;
+                else if ((x & 2) != 0)
+                    x++;
                 else
-                    x = x + 1;
+                    x--;
             }
             cnt++;
         }
